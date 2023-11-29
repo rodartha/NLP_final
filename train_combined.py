3d14e27095a9dcb2560f34b6fec26f7b1c1ab39e
Colin Page <colin.page@geospark.io>
2023-11-29 10:39:21 -0600

    training file for creating a new model trained on SNLI and ANLI

---
+++ train_combined.py
@@ -0,0 +1,126 @@
+import datasets
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, \
+    AutoModelForQuestionAnswering, Trainer, TrainingArguments, HfArgumentParser
+from helpers import prepare_dataset_nli, prepare_train_dataset_qa, \
+    prepare_validation_dataset_qa, QuestionAnsweringTrainer, compute_accuracy
+import os
+import json
+
+NUM_PREPROCESSING_WORKERS = 2
+
+
+def main():
+    argp = HfArgumentParser(TrainingArguments)
+    # The HfArgumentParser object collects command-line arguments into an object (and provides default values for unspecified arguments).
+    # In particular, TrainingArguments has several keys that you'll need/want to specify (when you call run.py from the command line):
+    # --do_train
+    #     When included, this argument tells the script to train a model.
+    #     See docstrings for "--task" and "--dataset" for how the training dataset is selected.
+    # --do_eval
+    #     When included, this argument tells the script to evaluate the trained/loaded model on the validation split of the selected dataset.
+    # --per_device_train_batch_size <int, default=8>
+    #     This is the training batch size.
+    #     If you're running on GPU, you should try to make this as large as you can without getting CUDA out-of-memory errors.
+    #     For reference, with --max_length=128 and the default ELECTRA-small model, a batch size of 32 should fit in 4gb of GPU memory.
+    # --num_train_epochs <float, default=3.0>
+    #     How many passes to do through the training data.
+    # --output_dir <path>
+    #     Where to put the trained model checkpoint(s) and any eval predictions.
+    #     *This argument is required*.
+
+    argp.add_argument('--model', type=str,
+                      default='google/electra-small-discriminator',
+                      help="""This argument specifies the base model to fine-tune.
+        This should either be a HuggingFace model ID (see https://huggingface.co/models)
+        or a path to a saved model checkpoint (a folder containing config.json and pytorch_model.bin).""")
+    argp.add_argument('--max_length', type=int, default=128,
+                      help="""This argument limits the maximum sequence length used during training/evaluation.
+        Shorter sequence lengths need less memory and computation time, but some examples may end up getting truncated.""")
+    argp.add_argument('--max_train_samples', type=int, default=None,
+                      help='Limit the number of examples to train on.')
+    argp.add_argument('--max_eval_samples', type=int, default=None,
+                      help='Limit the number of examples to evaluate on.')
+
+    training_args, args = argp.parse_args_into_dataclasses()
+
+    dataset_snli_id = "snli"
+    dataset_anli_id = "anli"
+    dataset_snli = datasets.load_dataset(*dataset_snli_id)
+    dataset_anli = datasets.load_dataset(*dataset_anli_id)
+    
+    # NLI models need to have the output label count specified (label 0 is "entailed", 1 is "neutral", and 2 is "contradiction")
+    task_kwargs = {'num_labels': 3}
+
+    # Here we select the right model fine-tuning head
+    model_classes = {'qa': AutoModelForQuestionAnswering,
+                     'nli': AutoModelForSequenceClassification}
+    model_class = model_classes['nli']
+    # Initialize the model and tokenizer from the specified pretrained model/checkpoint
+    model = model_class.from_pretrained(args.model, **task_kwargs)
+    tokenizer = AutoTokenizer.from_pretrained(args.model, use_fast=True)
+
+    # Select the dataset preprocessing function (these functions are defined in helpers.py)
+
+    prepare_train_dataset = prepare_eval_dataset = \
+        lambda exs: prepare_dataset_nli(exs, tokenizer, args.max_length)
+
+    print("Preprocessing data... (this takes a little bit, should only happen once per dataset)")
+
+    dataset_snli = dataset_snli.filter(lambda ex: ex['label'] != -1)
+    dataset_anli = dataset_anli.filter(lambda ex: ex['label'] != -1)
+    
+    train_dataset = None
+    eval_dataset = None
+    train_dataset_featurized = None
+    eval_dataset_featurized = None
+
+    train_dataset = datasets.concatenate_datasets([dataset_anli['train_r1'], dataset_anli['train_r2'], dataset_anli['train_r3'], dataset_snli['train']])
+    if args.max_train_samples:
+        train_dataset = train_dataset.select(range(args.max_train_samples))
+    train_dataset_featurized = train_dataset.map(
+        prepare_train_dataset,
+        batched=True,
+        num_proc=NUM_PREPROCESSING_WORKERS,
+        remove_columns=train_dataset.column_names
+    )
+
+    # Select the training configuration
+    trainer_class = Trainer
+    eval_kwargs = {}
+    # If you want to use custom metrics, you should define your own "compute_metrics" function.
+    # For an example of a valid compute_metrics function, see compute_accuracy in helpers.py.
+    compute_metrics = None
+
+    compute_metrics = compute_accuracy
+    
+
+    # This function wraps the compute_metrics function, storing the model's predictions
+    # so that they can be dumped along with the computed metrics
+    eval_predictions = None
+    def compute_metrics_and_store_predictions(eval_preds):
+        nonlocal eval_predictions
+        eval_predictions = eval_preds
+        return compute_metrics(eval_preds)
+
+    # Initialize the Trainer object with the specified arguments and the model and dataset we loaded above
+    trainer = trainer_class(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset_featurized,
+        eval_dataset=eval_dataset_featurized,
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics_and_store_predictions
+    )
+    # Train and/or evaluate
+    trainer.train()
+    trainer.save_model()
+    # If you want to customize the way the loss is computed, you should subclass Trainer and override the "compute_loss"
+    # method (see https://huggingface.co/transformers/_modules/transformers/trainer.html#Trainer.compute_loss).
+    #
+    # You can also add training hooks using Trainer.add_callback:
+    #   See https://huggingface.co/transformers/main_classes/trainer.html#transformers.Trainer.add_callback
+    #   and https://huggingface.co/transformers/main_classes/callback.html#transformers.TrainerCallback
+
+
+if __name__ == "__main__":
+    main()
